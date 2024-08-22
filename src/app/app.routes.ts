40e9195a825a0323ef1167c993a396734d7d68c1
lui15 <lpichardosanchez15@gmail.com>
2024-08-22 02:03:47 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'',redirectTo:'home',pathMatch:'full'},
+
+    {
+        path:'home',
+        loadChildren:() =>
+            import('./components/pages/home/home.component').then((m) => m.HomeComponent)
+    },
+
+    {
+        path:'character-list',
+        loadChildren:() => 
+            import('./components/pages/characters/character-list/character-list.component').then((m) =>
+            m.CharacterListComponent)
+    },
+
+    {
+        path:'character-details/:id',
+        loadChildren: () =>
+            import('./components/pages/characters/character-details/character-details.component').then((m) =>
+            m.CharacterDetailsComponent)
+    }
+];
+
+
---
+++ src/app/shared/components/form-search/form-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-search.component.html',
+  styleUrl: './form-search.component.css'
+})
+export class FormSearchComponent {
+
+}
